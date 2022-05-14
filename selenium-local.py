2dae2c6a99ec767570aff4d35528c3ae4128de82
Sebastian Villalobos <svillalobos@cognitiva.la>
2022-05-14 00:16:20 -0400

    clean the code

--- selenium-local.py
+++ selenium-local.py
@@ -6,9 +6,9 @@ current_path = Path().cwd()
 # Inicia el webdriver con el chromedriver como parametro
 driver = webdriver.Chrome(executable_path=f"{current_path}/chromedriver")
 
+# espera que cargue la pagina
+driver.implicitly_wait(0.5)
 
 driver.get("https://www.google.com")
 print(f"El titulo de la pagina es: {driver.title}") # => "Google"
-# espera que cargue la pagina
-driver.implicitly_wait(0.5)
 driver.quit()
